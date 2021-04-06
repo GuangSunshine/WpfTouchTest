d13a1d1bcf80673da4115fcf0633c10afd98c464
G@GitHub <yg.sunshine@gmail.com>
2021-04-06 17:29:26 -0500

    Initial commit
    Build on VS2019, run with a touch screen

---
+++ WpfAppTestTouchOnDialog/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTestTouchOnDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowSys_Click(object sender, RoutedEventArgs e)
+        {
+            var res = System.Windows.MessageBox.Show(this, "System.Windows.MessageBox", "System", MessageBoxButton.OKCancel);
+            Console.WriteLine($"MainWindow btnShowSys_Click, return res {res}");
+        }
+
+        private void btnShowCustomDialog_Click(object sender, RoutedEventArgs e)
+        {
+            simpleCustomWindow cusWin = new simpleCustomWindow();
+            cusWin.Owner = this;
+            var res = cusWin.ShowDialog();
+            Console.WriteLine($"MainWindow btnShowCustomDialog_Click simpleCustomWindow returned {res}");
+        }
+    }
+}
---
+++ WpfAppTestTouchOnDialog/simpleCustomWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppTestTouchOnDialog
+{
+    /// <summary>
+    /// Interaction logic for simpleCustomWindow.xaml
+    /// </summary>
+    public partial class simpleCustomWindow : Window
+    {
+        public simpleCustomWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimpleWindowOK_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine($"simpleCustomWindow btnSimpleWindowOK_Click, close");
+            Close();
+        }
+
+        private void btnSimpleWindowCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine($"simpleCustomWindow btnSimpleWindowCancel_Click, close");
+            Close();
+        }
+    }
+}
